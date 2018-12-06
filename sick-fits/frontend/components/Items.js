25379481e21493f6d95fd108edc7cd147311b5fc
Alby Bott <albybott@gmail.com>
2018-12-07 06:34:05 +1300

    Complete 16 - React Meets GraphQL

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import styled from "styled-components";
+
+import Item from "./Item";
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -15,18 +18,37 @@ const ALL_ITEMS_QUERY = gql`
   }
 `;
 
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxMidth};
+  margin: 0 auto;
+`;
+
 class Items extends Component {
   render() {
     return (
-      <div>
+      <Center>
         <p>Items</p>
         <Query query={ALL_ITEMS_QUERY}>
-          {payload => {
-            console.log(payload);
-            return <p>Hey Im the child of query</p>;
+          {({ data, error, loading }) => {
+            if (loading) return <p>loading...</p>;
+            if (error) return <p>{error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
           }}
         </Query>
-      </div>
+      </Center>
     );
   }
 }
